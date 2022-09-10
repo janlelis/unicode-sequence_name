2daba8c230b6cb61223aa3308d4f7de4a4bb3e3d
Jan Lelis <hi@ruby.consulting>
2022-09-10 10:04:21 +0200

    Add a Unicode 15.0 test case

--- spec/unicode_sequence_name_spec.rb
+++ spec/unicode_sequence_name_spec.rb
@@ -14,6 +14,7 @@ describe Unicode::SequenceName do
       assert_equal "MAN FEEDING BABY", Unicode::SequenceName.of("👨‍🍼") # Emoji 13.0
       assert_equal "HEART ON FIRE", Unicode::SequenceName.of("❤️‍🔥") # Emoji 13.1
       assert_equal "HANDSHAKE: LIGHT SKIN TONE, MEDIUM-DARK SKIN TONE", Unicode::SequenceName.of("🫱🏻‍🫲🏾") # Emoji 14.0
+      assert_equal "BLACK BIRD", Unicode::SequenceName.of("🐦‍⬛") # Emoji 15.0
     end
 
     it "will return nil for characters without name" do
