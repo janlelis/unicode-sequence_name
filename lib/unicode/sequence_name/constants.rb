9d709db64ceaaef13c114841a35be7496e49ab1d
Jan Lelis <hi@ruby.consulting>
2023-10-01 16:00:03 +0200

    Update Emoji version number

--- lib/unicode/sequence_name/constants.rb
+++ lib/unicode/sequence_name/constants.rb
@@ -4,7 +4,7 @@ module Unicode
   module SequenceName
     VERSION = "1.12.0"
     UNICODE_VERSION = "15.1.0"
-    EMOJI_VERSION = "15.0"
+    EMOJI_VERSION = "15.1"
     IVD_VERSION = "2022-09-13"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/sequence_name.marshal.gz").freeze
